2dd07274a8ae51b1c743bf781ef17540b35e893a
Himani <ihimanichawla@gmail.com>
2018-02-19 13:22:44 -0800

    Field Interpolation

---
+++ Proj2/proj2.cxx
@@ -0,0 +1,415 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    SpecularSpheres.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+//
+// This examples demonstrates the effect of specular lighting.
+//
+#include "vtkSmartPointer.h"
+#include "vtkSphereSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkInteractorStyle.h"
+#include "vtkObjectFactory.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkProperty.h"
+#include "vtkCamera.h"
+#include "vtkLight.h"
+#include "vtkOpenGLPolyDataMapper.h"
+#include "vtkJPEGReader.h"
+#include "vtkImageData.h"
+#include <iostream>
+#include <vtkPolyData.h>
+#include <vtkPointData.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPoints.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkFloatArray.h>
+#include <vtkDoubleArray.h>
+#include <vtkCellArray.h>
+#include <vtkDataSetReader.h>
+#include <vtkContourFilter.h>
+#include <vtkRectilinearGrid.h>
+#include <vtkFloatArray.h>
+#include <list>
+
+
+
+// ****************************************************************************
+//  Function: GetNumberOfPoints
+//
+//  Arguments:
+//     dims: an array of size 3 with the number of points in X, Y, and Z.
+//           2D data sets would have Z=1
+//
+//  Returns:  the number of points in a rectilinear mesh
+//
+// ****************************************************************************
+
+int GetNumberOfPoints(const int *dims)
+{
+    // 3D
+    //return dims[0]*dims[1]*dims[2];
+    // 2D
+    return dims[0]*dims[1];
+}
+
+// ****************************************************************************
+//  Function: GetNumberOfCells
+//
+//  Arguments:
+//
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the number of cells in a rectilinear mesh
+//
+// ****************************************************************************
+
+int GetNumberOfCells(const int *dims)
+{
+    // 3D
+    //return (dims[0]-1)*(dims[1]-1)*(dims[2]-1);
+    // 2D
+    return (dims[0]-1)*(dims[1]-1);
+}
+
+
+// ****************************************************************************
+//  Function: GetPointIndex
+//
+//  Arguments:
+//      idx:  the logical index of a point.
+//              0 <= idx[0] < dims[0]
+//              1 <= idx[1] < dims[1]
+//              2 <= idx[2] < dims[2] (or always 0 if 2D)
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the point index
+//
+// ****************************************************************************
+
+int GetPointIndex(const int *idx, const int *dims)
+{
+    // 3D
+    //return idx[2]*dims[0]*dims[1]+idx[1]*dims[0]+idx[0];
+    // 2D
+    return idx[1]*dims[0]+idx[0];
+}
+
+
+// ****************************************************************************
+//  Function: GetCellIndex
+//
+//  Arguments:
+//      idx:  the logical index of a cell.
+//              0 <= idx[0] < dims[0]-1
+//              1 <= idx[1] < dims[1]-1 
+//              2 <= idx[2] < dims[2]-1 (or always 0 if 2D)
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the cell index
+//
+// ****************************************************************************
+
+int GetCellIndex(const int *idx, const int *dims)
+{
+    // 3D
+    //return idx[2]*(dims[0]-1)*(dims[1]-1)+idx[1]*(dims[0]-1)*idx[0];
+    // 2D
+    return idx[1]*(dims[0]-1)+idx[0];
+}
+
+// ****************************************************************************
+//  Function: GetLogicalPointIndex
+//
+//  Arguments:
+//      idx (output):  the logical index of the point.
+//              0 <= idx[0] < dims[0]
+//              1 <= idx[1] < dims[1] 
+//              2 <= idx[2] < dims[2] (or always 0 if 2D)
+//      pointId:  a number between 0 and (GetNumberOfPoints(dims)-1).
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  None (argument idx is output)
+//
+// ****************************************************************************
+
+void GetLogicalPointIndex(int *idx, int pointId, const int *dims)
+{
+    // 3D
+    // idx[0] = pointId%dim[0];
+    // idx[1] = (pointId/dims[0])%dims[1];
+    // idx[2] = pointId/(dims[0]*dims[1]);
+
+    // 2D
+    idx[0] = pointId%dims[0];
+    idx[1] = pointId/dims[0];
+}
+
+
+// ****************************************************************************
+//  Function: GetLogicalCellIndex
+//
+//  Arguments:
+//      idx (output):  the logical index of the cell index.
+//              0 <= idx[0] < dims[0]-1
+//              1 <= idx[1] < dims[1]-1 
+//              2 <= idx[2] < dims[2]-1 (or always 0 if 2D)
+//      cellId:  a number between 0 and (GetNumberOfCells(dims)-1).
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  None (argument idx is output)
+//
+// ****************************************************************************
+
+void GetLogicalCellIndex(int *idx, int cellId, const int *dims)
+{
+    // 3D
+    // idx[0] = cellId%(dims[0]-1);
+    // idx[1] = (cellId/(dims[0]-1))%(dims[1]-1);
+    // idx[2] = cellId/((dims[0]-1)*(dims[1]-1));
+
+    // 2D
+    idx[0] = cellId%(dims[0]-1);
+    idx[1] = cellId/(dims[0]-1);
+}
+
+
+// ****************************************************************************
+//  Function: EvaluateFieldAtLocation
+//
+//  Arguments:
+//     pt: a two-dimensional location
+//     dims: an array of size two.  
+//              The first number is the size of the array in argument X, 
+//              the second the size of Y.
+//     X: an array (size is specified by dims).  
+//              This contains the X locations of a rectilinear mesh.
+//     Y: an array (size is specified by dims).  
+//              This contains the Y locations of a rectilinear mesh.
+//     F: a scalar field defined on the mesh.  Its size is dims[0]*dims[1].
+//
+//   Returns: the interpolated field value. 0 if the location is out of bounds.
+//
+// ****************************************************************************
+
+float interpolation_cal (const float pt, const int *dims, 
+                        const float *X, const float *Y, const float *F, int i , int j){
+	int left = (dims[0] * j) + (i-1);
+	int right = (dims[0] * (j)  ) + (i);
+	float temp2 = X[i] - X[i-1];
+	float temp1 = pt - X[i-1];
+	float t = (temp1/temp2);
+	float A = F[left];
+	float B = F[right];
+	float diff = B- A;
+	float result = ((A) + ( t * diff));
+	return result;
+}
+
+
+float
+EvaluateFieldAtLocation(const float *pt, const int *dims, 
+                        const float *X, const float *Y, const float *F)
+{
+	
+	int i =0;
+	int j =0;
+	for (i = 0; i < dims[0]; i++ ){
+		if (X[i] > pt[0])
+		break;
+	}
+	for (j = 0; j < dims[1]; j++ ){
+		if (Y[j] > pt[1])
+		break;
+	}
+	
+	if (i >= dims[0] || i ==0 || j >= dims[1] || j ==0){
+		return 0;
+	}
+	else
+	{
+		float f_up = interpolation_cal(pt[0],dims,X,Y, F, i,j);
+		float f_down = interpolation_cal(pt[0],dims,X, Y, F, i , j-1);
+		float diff1 = f_down - f_up;
+		float temp1 = Y[j-1] - Y[j];
+		float temp2 = pt[1] - Y[j];
+		float t = (temp2/temp1);
+		float result = (f_up + (t * diff1));
+		return result; 
+
+}
+}
+
+// ****************************************************************************
+//  Function: BoundingBoxForCell
+//
+//  Arguments:
+//     X: an array (size is specified by dims).  
+//              This contains the X locations of a rectilinear mesh.
+//     Y: an array (size is specified by dims).  
+//              This contains the Y locations of a rectilinear mesh.
+//     dims: an array of size two.  
+//              The first number is the size of the array in argument X, 
+//              the second the size of Y.
+//     cellId: a cellIndex (I.e., between 0 and GetNumberOfCells(dims))
+//     bbox (output): the bounding box of cellId.  Format should be
+ //                     bbox[0]: the minimum X value in cellId.
+//                     bbox[1]: the maximum X value in cellId.
+//                     bbox[2]: the minimum Y value in cellId.
+//                     bbox[3]: the maximum Y value in cellId.
+//
+//  Returns:  None (argument bbox is output)
+//
+// ****************************************************************************
+
+void
+BoundingBoxForCell(const float *X, const float *Y, const int *dims,
+                   int cellId, float *bbox)
+{
+	int cells[2];
+	GetLogicalCellIndex(cells, cellId, dims);
+	
+	int i = cells[0];
+	int j = cells[1];
+    if ((i+1) >= dims[0] || ( j+1) >= dims[1]){
+    	bbox[0] = -100;
+    	bbox[1] = +100;
+    	bbox[2] = -100;
+    	bbox[3] = +100;
+	}
+	else{
+   
+    	bbox[0] = X[i];
+    	bbox[1] = X[i + 1];
+    	bbox[2] = Y[j];
+    	bbox[3] = Y[j + 1];
+	}
+}
+
+// ****************************************************************************
+//  Function: CountNumberOfStraddingCells
+//
+//  Arguments:
+//     X: an array (size is specified by dims).  
+//              This contains the X locations of a rectilinear mesh.
+//     Y: an array (size is specified by dims).  
+//              This contains the Y locations of a rectilinear mesh.
+//     dims: an array of size two.  
+//              The first number is the size of the array in argument X, 
+//              the second the size of Y.
+//     F: a scalar field defined on the mesh.  Its size is dims[0]*dims[1].
+//
+//  Returns:  the number of cells that straddle 0, i.e., the number of cells
+//            that contains points who have F>0 and also have points with F<0.
+//
+// ****************************************************************************
+
+int
+CountNumberOfStraddlingCells(const float *X, const float *Y, const int *dims,
+                             const float *F)
+{
+	
+	int num_cells = (dims[0]-1)*(dims[1]-1);
+	int count = num_cells;
+	for(int k = 0; k < num_cells; k++ ){
+		int cells[2];
+    	GetLogicalCellIndex(cells, k , dims);
+    	int i = cells[0];
+    	int j = cells[1];
+    	int p1 = (dims[0] * j) + i;
+    	int p2 = (dims[0] * (j+1) ) + i;
+    	int p3 = (dims[0] * j) + (i+1);
+    	int p4 = (dims[0]*(j+1) )+ (i+1);
+    	float a = F[p1];
+    	float b = F[p2];
+    	float c = F[p3];
+    	float d = F[p4];
+    	if (a > 0 && b >0 && c >0 && d>0 || a < 0 && b< 0 && c <0 && d < 0 )
+    		count --;
+ 	}
+return count ; 
+}
+
+int main()
+{
+    int  i;
+
+    vtkDataSetReader *rdr = vtkDataSetReader::New();
+    rdr->SetFileName("proj2_data.vtk");
+    rdr->Update();
+
+    int dims[3];
+    vtkRectilinearGrid *rgrid = (vtkRectilinearGrid *) rdr->GetOutput();
+    rgrid->GetDimensions(dims);
+
+    float *X = (float *) rgrid->GetXCoordinates()->GetVoidPointer(0);
+    float *Y = (float *) rgrid->GetYCoordinates()->GetVoidPointer(0);
+    float *F = (float *) rgrid->GetPointData()->GetScalars()->GetVoidPointer(0);
+    
+    int numCells = CountNumberOfStraddlingCells(X, Y, dims, F);
+    cerr << "The number of cells straddling zero is " << numCells << endl;
+
+    float bbox[4];
+    const int ncells = 5;
+    int cellIds[ncells] = { 0, 50, 678, 1000, 1200 };
+    for (i = 0 ; i < ncells ; i++)
+    {
+        BoundingBoxForCell(X, Y, dims, cellIds[i], bbox);
+        cerr << "The bounding box for cell " << cellIds[i] << " is " 
+             << bbox[0] << "->" << bbox[1] << ", " << bbox[2] << "->" << bbox[3]
+             << endl;
+    }
+
+    const int npts = 10;
+    float pt[npts][3] = 
+         {
+            {1.01119, 0.122062, 0},
+            {0.862376, 1.33839, 0},
+            {0.155026, 0.126123, 0},
+            {0.69736, 0.0653565, 0},
+            {0.2, 0.274117, 0},
+            {0.893699, 1.04111, 0},
+            {0.608791, -0.0533753, 0},
+            {1.00543, 0.138024, 0},
+            {0.384128, -0.0768977, 0},
+            {0.666757, 0.60259, 0},
+         };
+
+    
+
+    for (i = 0 ; i < npts ; i++)
+    {
+        float f = EvaluateFieldAtLocation(pt[i], dims, X, Y, F);
+        cerr << "Evaluated field at (" << pt[i][0] <<"," << pt[i][1] << ") as "
+             << f << endl;
+    }
+    
+   
+  //  cerr << "Infinite loop here, else Windows people may have the terminal "
+    //     << "disappear before they see the output."
+    //     << " Remove these lines if they annoy you." << endl;
+   // cerr << "(press Ctrl-C to exit program)" << endl;
+    //while (1) ; 
+}
+
+
+
+
